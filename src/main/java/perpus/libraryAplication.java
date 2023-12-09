c71cc02f8b696d87e89794a8539e9b034db044e2
hsdiqi <hasbi.a.s345@gmail.com>
2023-12-09 14:41:04 +0700

    change code and rename

--- src/main/java/perpus/libraryAplication.java
+++ src/main/java/perpus/libraryAplication.java
@@ -2,17 +2,16 @@ package perpus;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class libraryAplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root);
+        FXMLLoader fxmlLoader = new FXMLLoader(libraryAplication.class.getResource("login.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 360, 640);
         stage.setTitle("Anjay Mabar");
         stage.setScene(scene);
         stage.show();
