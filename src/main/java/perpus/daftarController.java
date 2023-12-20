b03ac7fce19025ded43068cb3b515fd339822bff
Andika Salsabilah <andika.as995@gmail.com>
2023-12-20 23:04:55 +0700

    added change scene for daftarController and logincontroller. (ganti scene misal click link masuk/daftar)

--- src/main/java/perpus/daftarController.java
+++ src/main/java/perpus/daftarController.java
@@ -1,9 +1,15 @@
 package perpus;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.regex.*;
 public class daftarController {
@@ -17,6 +23,8 @@ public class daftarController {
     public TextField tfPassword;
     @FXML
     public Label alertText;
+    @FXML
+    private Hyperlink btnMasuk;
     @FXML
     protected void btnDaftar(){
         String nama = tfNama.getText();
@@ -38,6 +46,21 @@ public class daftarController {
             }
         }else{
             alertText.setText("Kolom tidak boleh kosong!!");
+        }
+    }
+    @FXML
+    protected void ChangeScene() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) btnMasuk.getScene().getWindow();
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
--- src/main/java/perpus/logincontroller.java
+++ src/main/java/perpus/logincontroller.java
@@ -1,10 +1,16 @@
 package perpus;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import perpus.validasi;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class logincontroller {
 
@@ -14,6 +20,8 @@ public class logincontroller {
     public TextField usNameText;
     @FXML
     public Label alertText;
+    @FXML
+    private Hyperlink btnDaftar;
 
     @FXML
     protected void btnLogin() {
@@ -34,6 +42,17 @@ public class logincontroller {
     }
 
     @FXML
-    public void linkSignClick() {
+    protected void ChangeScene() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("daftar.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) btnDaftar.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
