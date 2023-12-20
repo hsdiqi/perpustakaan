ff221da0e83988b4abd7db3e26acf3aa3fb9943d
Andika Salsabilah <andika.as995@gmail.com>
2023-12-20 21:25:24 +0700

    edit daftarController + create User class

---
+++ src/main/java/perpus/User.java
@@ -0,0 +1,8 @@
+package perpus;
+
+public class User {
+    public String nama;
+    public String username;
+    public String email;
+    public String password;
+}
--- src/main/java/perpus/daftarController.java
+++ src/main/java/perpus/daftarController.java
@@ -3,6 +3,8 @@ package perpus;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.sql.*;
 import java.util.regex.*;
 public class daftarController {
     @FXML
@@ -24,6 +26,12 @@ public class daftarController {
 
         if (!nama.isEmpty() && !username.isEmpty() && !password.isEmpty() && !email.isEmpty()){
             if (cekEmail(email)){
+                user = addUserToDatabase(nama,username,email,password);
+                if (user != null){
+                    System.out.println("Berhasil mendaftarkan " + user.nama);
+                }else{
+                    System.out.println("Gagal melakukan pendaftaran");
+                }
                 alertText.setText("Pendaftaran Berhasil!!!");
             }else{
                 alertText.setText("Email invalid");
@@ -31,6 +39,38 @@ public class daftarController {
         }else{
             alertText.setText("Kolom tidak boleh kosong!!");
         }
+    }
+
+    public static User user;
+    private User addUserToDatabase(String nama, String username, String email, String password){
+        User user = null;
+        try {
+            Connection connection = DatabaseConnector.connect();
+            Statement statement = connection.createStatement();
+            String sqlQuery = "INSERT INTO users (nama, username, email, password)"+"VALUES (?,?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, nama);
+            preparedStatement.setString(2,username);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, password);
+
+            //tambah row ke table
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0){
+                user = new User();
+                user.nama = nama;
+                user.email = email;
+                user.username = username;
+                user.password = password;
+            }
+
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
     private boolean cekEmail(String email) {
@@ -43,6 +83,8 @@ public class daftarController {
             return false;
         }
     }
+
+
 
 
 }
