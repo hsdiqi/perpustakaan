8ed39d12a4e4f1d787783cd274b6edee1a84561e
hsdiqi <hasbi.a.s345@gmail.com>
2023-12-25 15:12:08 +0700

    menambahkan datasesi2 / magementsesion

---
+++ src/main/java/perpus/DataSesi2.java
@@ -0,0 +1,22 @@
+package perpus;
+
+public class DataSesi2 {
+    public static String username;
+    public static int userId;
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        DataSesi2.username = username;
+    }
+
+    public static int getUserId() {
+        return userId;
+    }
+
+    public static void setUserId(int userId) {
+        DataSesi2.userId = userId;
+    }
+}
